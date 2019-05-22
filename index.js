8af0e6aa7838e6d4e0b12ca5a87fd4b49a04d2ca
OctopiWithHats <sdgranoff@gmail.com>
2019-05-22 13:41:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code here
+//class Breakfast with food and drink
+class Breakfast{
+  constructor(food, drink){
+    this.food = food;
+    this.drink = drink;
+  }
+}
+//class Lunch with salad, soup, drink
+class Lunch{
+  constructor(salad, soup, drink){
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+//class Dinner with salad, soup, entree, private dessert
+class Dinner{
+  constructor(salad, soup, entree, dessert){
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
+  }
+}
