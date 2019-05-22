43363f491082c6e5523f15bab3e3d2f8f659ac2f
William Sinn <w.h.sinn@gmail.com>
2019-05-22 14:26:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code here
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+new Breakfast('egg', 'juice')
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink
+  }
+}
+new Lunch('side salad', 'broccoli cheddar', 'iced tea')
+
+class Dinner {
+  constructor(salad, soup, entree, _dessert){
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = _dessert;
+  }
+}
+
+new Dinner('balsamic salad', 'consomme', 'filet mignon', 'cheesecake')
